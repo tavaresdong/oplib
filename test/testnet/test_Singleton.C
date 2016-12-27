bc48f4c1bc18dbd58907d8ba09f17f7153d28a9c
tavaresdong <tavares.nju@gmail.com>
2016-12-27 22:48:57 +0800

    Add CurrentThread::tid and UNLIKELY()

--- test/testnet/test_Singleton.C
+++ test/testnet/test_Singleton.C
@@ -1,6 +1,8 @@
 #include <net/Singleton.H>
+#include <net/Thread.H>
 #include <vector>
 #include <iostream>
+#include <string>
 
 int main()
 {
@@ -10,6 +12,11 @@ int main()
   auto pvec2 = oplib::Singleton<std::vector<int>>::instance();
 
   pvec->push_back(1);
+
+  int tid = oplib::CurrentThread::tid();
+  std::string tidString = oplib::CurrentThread::tidString();
+
+  std::cout << tidString << std::endl;
 
   std::cout << pvec2->size() << std::endl;
   return 0;
