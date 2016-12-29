246837c7c1430b85a23b61b446c255c43d2edc52
tavaresdong <tavares.nju@gmail.com>
2016-12-29 20:43:55 +0800

    Singleton tested

--- test/testnet/test_Singleton.C
+++ test/testnet/test_Singleton.C
@@ -1,23 +1,80 @@
 #include <net/Singleton.H>
 #include <net/Thread.H>
-#include <vector>
-#include <iostream>
-#include <string>
+
+#include <stdio.h>
+#include <unistd.h>
+
+using namespace oplib;
+
+class Test
+{
+ public:
+  Test(const Test& rhs_) = delete;
+  Test& operator=(const Test& rhs_) = delete;
+
+  Test()
+  {
+    printf("tid=%d, constructing %p\n", CurrentThread::tid(), this);
+  }
+
+  ~Test()
+  {
+    printf("tid=%d, destructing %p %s\n", CurrentThread::tid(), this, _name.c_str());
+  }
+
+  const std::string& name() const { return _name; }
+  void setName(const std::string& n) { _name = n; }
+
+ private:
+  std::string _name;
+};
+
+void threadFunc()
+{
+  printf("tid=%d, %p name=%s\n",
+         CurrentThread::tid(),
+         Singleton<Test>::instance(),
+         Singleton<Test>::instance()->name().c_str());
+  Singleton<Test>::instance()->setName("only one, changed");
+}
+
+template<unsigned N>
+class Destruct
+{
+ public:
+  Destruct()
+  {
+    printf("construct %d\n", N);
+    more();
+  }
+
+  ~Destruct()
+  {
+    printf("destruct %d\n", N);
+  }
+
+  void more()
+  {
+    Singleton<Destruct<N-1> >::instance();
+  }
+};
+
+template<>
+void Destruct<0>::more()
+{
+}
 
 int main()
 {
+  Singleton<Test>::instance()->setName("only one");
+  Thread t1(threadFunc);
+  t1.start();
+  t1.join();
+  printf("tid=%d, %p name=%s\n",
+         CurrentThread::tid(),
+         Singleton<Test>::instance(),
+         Singleton<Test>::instance()->name().c_str()); // Should be changed
 
-  // TODO test later, stub here
-  auto pvec = oplib::Singleton<std::vector<int>>::instance();
-  auto pvec2 = oplib::Singleton<std::vector<int>>::instance();
-
-  pvec->push_back(1);
-
-  int tid = oplib::CurrentThread::tid();
-  std::string tidString = oplib::CurrentThread::tidString();
-
-  std::cout << tidString << std::endl;
-
-  std::cout << pvec2->size() << std::endl;
-  return 0;
+  Singleton<Destruct<100> >::instance();
+  printf("ATEXIT_MAX = %ld\n", sysconf(_SC_ATEXIT_MAX));
 }
