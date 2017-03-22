e9e352ccf7674d1bdfd455957399123eb8bb6c18
tavaresdong <tavares.nju@gmail.com>
2017-03-22 16:40:52 +0800

    RBTree insert & testcase

--- lib/ds/RBTreeT.C
+++ lib/ds/RBTreeT.C
@@ -1,3 +1,5 @@
+#include <iostream>
+
 // There is a header guard: red, root's parent is header 
 // and header's parent is root. header's left is the leftmost node,
 // header's right is the rightmost node
@@ -30,13 +32,14 @@ void RBIterator<Value, Ref, Ptr>::decrement()
   if (_pnode->_color == RBTreeNodeColor::Red &&
       _pnode->_parent->_parent == _pnode)
   {
-    // _pnode is at Header guard: header's parent is root, root's parent is header.
-    // and root must be black, header is red
+    // _pnode is header: header's parent is root, root's parent is header.
+    // and root must be black, header is red, so _pnode is end()
+    // --end() == the last element
     _pnode = _pnode->_right;
   }
   if (_pnode->_left != nullptr)
   {
-    return RBNodeBase::maxnode(_pnode->_left);
+    _pnode = RBNodeBase::maxnode(_pnode->_left);
   }
   else
   {
@@ -63,7 +66,333 @@ template <typename Key, typename Value, class KeyOfValue,
 RBTree<Key, Value, KeyOfValue, Comp, Alloc>::RBTree(const Comp& comp_)
 : _allocator(NodeAlloc()),
   _nodeCount(0),
-  _comparator(comp_)
+  _comparator(comp_),
+  _keyExtractor(KeyOfValue())
 {
   init();
+}
+
+template <typename Key, typename Value, class KeyOfValue,
+          typename Comp, template <typename> class Alloc>
+typename RBTree<Key, Value, KeyOfValue, Comp, Alloc>::iterator
+RBTree<Key, Value, KeyOfValue, Comp, Alloc>::insertEqual(const value_type& val_)
+{
+  NodePtr par = _header;
+  NodePtr cur = root();
+
+  while (cur != nullptr)
+  {
+    par = cur;
+    if (_comparator(_keyExtractor(val_), key(cur)))
+    {
+      cur = cur->_left;
+    }
+    else
+    {
+      cur = cur ->_right;
+    }
+  }
+
+  return insert(static_cast<NodePtr>(cur),
+                static_cast<NodePtr>(par),
+                val_);
+}
+
+template <typename Key, typename Value, class KeyOfValue,
+          typename Comp, template <typename> class Alloc>
+typename RBTree<Key, Value, KeyOfValue, Comp, Alloc>::iterator
+RBTree<Key, Value, KeyOfValue, Comp, Alloc>::insert(NodePtr child_, NodePtr parent_, const value_type& val_)
+{
+  auto newNode = createNode(val_);
+
+  if (parent_ == _header)
+  {
+    // Case1: empty tree
+    parent_->_left = newNode;
+    parent_->_right = newNode;
+    parent_->_parent = newNode;
+  }
+  else if (_comparator(_keyExtractor(val_), key(parent_)))
+  {
+    parent_->_left = newNode;
+    if (parent_ == leftmost())
+    {
+      // Assign leftmost to the new node
+      _header->_left = newNode;
+    }
+  }
+  else
+  {
+    parent_->_right = newNode;
+    if (parent_ == rightmost())
+    {
+      // Assign rightmost to the new node
+      _header->_right = newNode;
+    }
+  }
+
+  // Initialize the newly created node:
+  newNode->_left = newNode->_right = nullptr;
+  newNode->_parent = parent_;
+
+  rebalance(newNode, _header->_parent);
+  ++_nodeCount;
+  return iterator(newNode);
+}
+
+template <typename Key, typename Value, class KeyOfValue,
+          typename Comp, template <typename> class Alloc>
+std::pair<typename RBTree<Key, Value, KeyOfValue, Comp, Alloc>::iterator, bool>
+RBTree<Key, Value, KeyOfValue, Comp, Alloc>::insertUnique(const value_type& val_)
+{
+ // This implementation is OK, but costs two invocations of _comparator, 
+ // which is not efficient!
+ //
+ // NodePtr _par = _header;
+ // NodePtr _cur = root();
+
+ // while (_cur != nullptr)
+ // {
+ //   _par = _cur;
+ //   if (_comparator(_keyExtractor(val_), key(_cur)))
+ //   {
+ //     _cur = _cur->_left;
+ //   }
+ //   else if (_comparator(key(_cur), _keyExtractor(val_)))
+ //   {
+ //     _cur = _cur ->_right;
+ //   }
+ //   else
+ //   {
+ //     // Value of current node equals the val_ to insert
+ //     return std::make_pair(iterator(_cur), false);
+ //   }
+ // }
+
+ // return std::make_pair(insert(static_cast<NodePtr>(_cur),
+ //                       static_cast<NodePtr>(_par),
+ //                       val_), true);
+  NodePtr par = _header;
+  NodePtr cur = root();
+  bool comp = true;
+  while (cur != nullptr)
+  {
+    par = cur;
+    comp = _comparator(_keyExtractor(val_), key(cur));
+    cur = comp ? left(cur) : right(cur);
+  }
+
+  iterator it { par };
+  if (comp)
+  {
+    if (it == begin())
+      return std::make_pair(insert(static_cast<NodePtr>(cur), 
+                                   static_cast<NodePtr>(par),
+                                   val_), true);
+    else
+      --it; // The last element along the path that has value <= val_
+  }
+
+  if (_comparator(key(static_cast<NodePtr>(it._pnode)),
+                  _keyExtractor(val_)))
+    return std::make_pair(insert(static_cast<NodePtr>(cur),
+                                 static_cast<NodePtr>(par),
+                                 val_), true);
+
+  return std::make_pair(it, false);
+}
+
+template <typename Key, typename Value, class KeyOfValue,
+          typename Comp, template <typename> class Alloc>
+void RBTree<Key, Value, KeyOfValue, Comp, Alloc>::rebalance(RBNodeBase* toInsert_, RBNodeBase*& root_)
+{
+  // RBTree: the count of black nodes along all paths should be the same,
+  // so the inserted node must be red.
+  // If its parent is not black, then we need to do rotation
+  toInsert_->_color = RBTreeNodeColor::Red;
+  while (toInsert_ != root_ && toInsert_->_parent->_color == RBTreeNodeColor::Red)
+  {
+    auto parent = toInsert_->_parent;
+    auto grandpa = parent->_parent;
+
+    if (parent == grandpa->_left)
+    {
+      // Sibling of father node
+      auto uncle = grandpa->_right;
+      if (uncle == nullptr || color(uncle) == RBTreeNodeColor::Black)
+      {
+        if (toInsert_ == parent->_right)
+        {
+          // Left rotate first
+          toInsert_ = parent;
+          leftRotate(toInsert_, root_);
+          parent = toInsert_->_parent;
+        }
+        parent->_color = RBTreeNodeColor::Black;
+        grandpa->_color = RBTreeNodeColor::Red;
+        rightRotate(grandpa, root_);
+      }
+      else
+      {
+        parent->_color = RBTreeNodeColor::Black;
+        uncle->_color = RBTreeNodeColor::Black;
+        grandpa->_color = RBTreeNodeColor::Red;
+        toInsert_ = grandpa;  // Needs to check upwards
+      }
+    }
+    else
+    {
+      auto uncle = toInsert_->_parent->_parent->_left;
+      if (uncle == nullptr || color(uncle) == RBTreeNodeColor::Black)
+      {
+        if (toInsert_ == grandpa->_left)
+        {
+          toInsert_ = parent;
+          rightRotate(toInsert_, root_);
+          parent = toInsert_->_parent;
+        }
+        parent->_color = RBTreeNodeColor::Black;
+        grandpa->_color = RBTreeNodeColor::Red;
+        leftRotate(grandpa, root_);
+      }
+      else
+      {
+        parent->_color = RBTreeNodeColor::Black;
+        uncle->_color = RBTreeNodeColor::Black;
+        grandpa->_color = RBTreeNodeColor::Red;
+        toInsert_ = grandpa;
+      }
+    }
+  }
+
+  // The root node is always black
+  root_->_color = RBTreeNodeColor::Black;
+}
+
+template <typename Key, typename Value, class KeyOfValue,
+          typename Comp, template <typename> class Alloc>
+void RBTree<Key, Value, KeyOfValue, Comp, Alloc>::leftRotate(RBNodeBase* node_, RBNodeBase*& root_)
+{
+  bool isRoot = (node_ == root_);
+  auto parent = node_->_parent;
+
+  // Could be neither(consider the root case, guarded by isRoot
+  bool isLeft = (node_ == parent->_left) ? true : false;
+
+  auto newRoot = node_->_right;
+  node_->_right = newRoot->_left;
+
+  if (node_->_right != nullptr)
+    node_->_right->_parent = node_;
+
+  node_->_parent = newRoot;
+  newRoot->_left = node_;
+  newRoot->_parent = parent;
+
+  if (isRoot) root_ = newRoot;
+  else if (isLeft) parent->_left = newRoot;
+  else parent->_right = newRoot;
+}
+
+template <typename Key, typename Value, class KeyOfValue,
+          typename Comp, template <typename> class Alloc>
+void RBTree<Key, Value, KeyOfValue, Comp, Alloc>::rightRotate(RBNodeBase* node_, RBNodeBase*& root_)
+{
+  bool isRoot = (node_ == root_);
+  auto parent = node_->_parent;
+
+  bool isLeft = (node_ == parent->_left) ? true : false;
+
+  auto newRoot = node_->_left;
+  node_->_left = newRoot->_right;
+
+  if (node_->_left != nullptr)
+    node_->_left->_parent = node_;
+
+  node_->_parent = newRoot;
+  newRoot->_right = node_;
+  newRoot->_parent = parent;
+
+  if (isRoot) root_ = newRoot;
+  else if (isLeft) parent->_left = newRoot;
+  else parent->_right = newRoot;
+}
+
+namespace detail
+{
+  bool isLeaf(RBNodeBase* node_)
+  { return node_->_left == nullptr && node_->_right == nullptr; }
+
+  bool hasLeft(RBNodeBase* node_)
+  { return node_->_left != nullptr; }
+
+  bool hasRight(RBNodeBase* node_)
+  { return node_->_right != nullptr; }
+
+  void calculateBlackDepth(RBNodeBase* node_, std::vector<int>& depths_, int curDepth)
+  {
+    if (node_ == nullptr) return;
+    if (node_->_color == RBTreeNodeColor::Black) ++curDepth;
+    if (isLeaf(node_)) depths_.push_back(curDepth);
+    if (hasLeft(node_)) calculateBlackDepth(node_->_left, depths_, curDepth);
+    if (hasRight(node_)) calculateBlackDepth(node_->_right, depths_, curDepth);
+  }
+
+  bool noConsecutiveReds(RBNodeBase* node_)
+  {
+    if (node_ == nullptr) return true;
+    if (isLeaf(node_)) return true;
+    if (node_->_color == RBTreeNodeColor::Red)
+    {
+      if (hasLeft(node_) &&
+          node_->_left->_color == RBTreeNodeColor::Red)
+      {
+        return false;
+      }
+      if (hasRight(node_) &&
+          node_->_right->_color == RBTreeNodeColor::Red)
+      {
+        return false;
+      }
+    }
+
+    if (!noConsecutiveReds(node_->_left)) return false;
+    return noConsecutiveReds(node_->_right);
+  }
+}
+
+template <typename Key, typename Value, class KeyOfValue,
+          typename Comp, template <typename> class Alloc>
+bool RBTree<Key, Value, KeyOfValue, Comp, Alloc>::rbPropertyKept() const
+{
+  // (1) Header color: red
+  if (_header->_color != RBTreeNodeColor::Red)
+  {
+    return false;
+  }
+
+  // (2) root node(if exist) must be Black
+  if (_header->_parent != nullptr &&
+      _header->_parent->_color != RBTreeNodeColor::Black)
+  {
+    return false;
+  }
+
+  // (3) All path have the same black count
+  std::vector<int> blackDepths;
+  detail::calculateBlackDepth(_header->_parent, blackDepths, 0);
+  if (!blackDepths.empty())
+  {
+    auto depth = blackDepths.front();
+    for (const auto& d : blackDepths)
+    {
+      if (d != depth)
+      {
+        return false;
+      }
+    }
+  }
+
+  // (4) No two consecutive (parent/child) node are all red
+  return detail::noConsecutiveReds(_header->_parent);
 }
--- test/testds/testRBTree.cc
+++ test/testds/testRBTree.cc
@@ -3,13 +3,23 @@
 #include <string>
 #include <cstdlib>
 #include <iterator>
+#include <functional>
+#include <iostream>
 
 namespace 
 {
 class Comparator
 {
+public:
   bool operator() (int v1, int v2)
   { return v1 < v2; };
+};
+
+class KeyOfValue
+{
+public:
+  int operator() (int v)
+  { return v; }
 };
 }
 
@@ -32,9 +42,36 @@ void RBTreeTest::TearDown() {}
 
 TEST_F(RBTreeTest, testCreation)
 {
-  auto kofv = [](int i ) { return i; };
-  oplib::ds::RBTree<int, int, decltype(kofv), Comparator>  rbtree;
+  oplib::ds::RBTree<int, int, KeyOfValue, Comparator> rbtree;
   EXPECT_TRUE(rbtree.empty());
   EXPECT_EQ(rbtree.size(), 0u);
+}
+
+TEST_F(RBTreeTest, testInsertion)
+{
+  oplib::ds::RBTree<int, int, KeyOfValue, std::less<int>> rbtree;
+  EXPECT_TRUE(rbtree.empty());
+  EXPECT_EQ(rbtree.size(), 0u);
+
+  rbtree.insertUnique(10); EXPECT_TRUE(rbtree.rbPropertyKept());
+  rbtree.insertUnique(7);  EXPECT_TRUE(rbtree.rbPropertyKept()); 
+  rbtree.insertUnique(8);  EXPECT_TRUE(rbtree.rbPropertyKept());
+  rbtree.insertUnique(15); EXPECT_TRUE(rbtree.rbPropertyKept());
+  rbtree.insertUnique(5);  EXPECT_TRUE(rbtree.rbPropertyKept());
+  rbtree.insertUnique(6);  EXPECT_TRUE(rbtree.rbPropertyKept());
+  rbtree.insertUnique(11); EXPECT_TRUE(rbtree.rbPropertyKept());
+  rbtree.insertUnique(13); EXPECT_TRUE(rbtree.rbPropertyKept());
+  rbtree.insertUnique(12); EXPECT_TRUE(rbtree.rbPropertyKept());
+
+  EXPECT_FALSE(rbtree.insertUnique(12).second);
+  EXPECT_EQ(rbtree.size(), 9u);
+
+  auto first = rbtree.begin();
+  auto second = first;
+  ++second;
+  for (; second != rbtree.end(); ++first, ++second)
+  {
+    EXPECT_TRUE(*first < *second);
+  }
 }
 
